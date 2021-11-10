84110353176a0591d62b57dabcf00a3cc2dca8ad
ravigupta44 <raviguptaamba4@gmail.com>
2021-11-10 08:59:00 +0530

    replacing html pages to react app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import NavBar from "./Navbar";
+import Footer from "./Footer";
+import HeroSection from "./HeroSection";
+import AppSection from "./AppSection";
+import CardSection from "./CardSection";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App =()=>{
+    return(
+        <div>
+            
+            <NavBar/>
+            
+            <HeroSection/>
+            <AppSection/>
+            <CardSection/>
+            <Footer/>
+        </div>
+    );
+};
 
 export default App;
